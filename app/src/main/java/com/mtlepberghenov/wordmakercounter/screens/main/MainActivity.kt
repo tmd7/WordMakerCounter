cfa62e0a7309a378958f4bdded933e8101d542c6
mtlepberghenov <mur.marat@gmail.com>
2019-02-05 17:01:31 +0200

    init commit

---
+++ app/src/main/java/com/mtlepberghenov/wordmakercounter/screens/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mtlepberghenov.wordmakercounter.screens.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mtlepberghenov.wordmakercounter.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
